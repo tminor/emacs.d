0cd418db85bea8b7be2ddc1323cd143dd9dc02da
tminor <thomas.minor@usg.edu>
2019-06-21 12:36:14 -0400

    straight: Fix org loading
    (require 'org) in init.el was causing the old version of org to load;
    this created load problems and broke emacs --daemon.

--- init.el
+++ init.el
@@ -11,9 +11,18 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+(straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
-(require 'org)
+;; HACK: (require 'org) loads the version of org included with Emacs;
+;; This interferes with loading packages that depend on org and can
+;; cause problems. Instead, this calls `use-package' (and thereby
+;; `straight-use-package' so that the most recent version of org is
+;; loaded.
+(use-package org
+  :straight org-plus-contrib
+  :demand t)
+
 (org-babel-load-file
  (expand-file-name "~/.emacs.d/main.org"))
 
